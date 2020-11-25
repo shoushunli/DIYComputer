5a174bdece527d1d01fa6235932af4d5a842d90e
Wenhao Yan <wenhaoy3@illinois.edu>
2020-11-25 15:36:30 +0800

    Fix duplicate insertions to dropdown-content memu

--- application/views/basicFunc.php
+++ application/views/basicFunc.php
@@ -12,90 +12,90 @@
 </head>
 
 <body>
-  	<div class="navbar">
+  <div class="navbar">
 
 
-  <a href="Home/"> <i class="fa fa-home"></i> Home</a>
+    <a href="Home/"> <i class="fa fa-home"></i> Home</a>
 
 
-  <div class="dropdown">
-    <button onclick = "myFunction('CPU', 'cpuDrop')" class="dropbtn">CPU <i class="fa fa-caret-down"></i></button>
-    <div id="cpuDrop" class="dropdown-content">
-      <input type="text" placeholder="Search.." id="cpuInput" onkeyup="filterFunction('cpuInput', 'cpuDrop')">
+    <div class="dropdown">
+      <button onclick="myFunction('CPU', 'cpuDrop')" class="dropbtn">CPU <i class="fa fa-caret-down"></i></button>
+      <div id="cpuDrop" class="dropdown-content">
+        <input type="text" placeholder="Search.." id="cpuInput" onkeyup="filterFunction('cpuInput', 'cpuDrop')">
 
+      </div>
     </div>
+
+    <div class="dropdown">
+      <button onclick="myFunction('Motherboard', 'mbDrop')" class="dropbtn">Motherboard <i class="fa fa-caret-down"></i></button>
+      <div id="mbDrop" class="dropdown-content">
+        <input type="text" placeholder="Search.." id="mbInput" onkeyup="filterFunction('mbInput', 'mbDrop')">
+
+      </div>
+    </div>
+
+    <div class="dropdown">
+      <button onclick="myFunction('Memory', 'mmDrop')" class="dropbtn">Memory <i class="fa fa-caret-down"></i></button>
+      <div id="mmDrop" class="dropdown-content">
+        <input type="text" placeholder="Search.." id="mmInput" onkeyup="filterFunction('mmInput','mmDrop')">
+
+      </div>
+    </div>
+
+    <div class="dropdown">
+      <button onclick="myFunction('GPU', 'gpuDrop')" class="dropbtn">GPU <i class="fa fa-caret-down"></i></button>
+      <div id="gpuDrop" class="dropdown-content">
+        <input type="text" placeholder="Search.." id="gpuInput" onkeyup="filterFunction('gpuInput','gpuDrop')">
+
+      </div>
+    </div>
+
+    <div class="dropdown">
+      <button onclick="myFunction('SSD', 'ssdDrop')" class="dropbtn">SSD <i class="fa fa-caret-down"></i></button>
+      <div id="ssdDrop" class="dropdown-content">
+        <input type="text" placeholder="Search.." id="ssdInput" onkeyup="filterFunction('ssdInput', 'ssdDrop')">
+
+      </div>
+    </div>
+
+    <div class="dropdown">
+      <button onclick="myFunction('HDD', 'hddDrop')" class="dropbtn">HDD <i class="fa fa-caret-down"></i></button>
+      <div id="hddDrop" class="dropdown-content">
+        <input type="text" placeholder="Search.." id="hddInput" onkeyup="filterFunction('hddInput', 'hddDrop')">
+
+      </div>
+    </div>
+
+    <div class="dropdown">
+      <button onclick="myFunction('Case', 'caseDrop')" class="dropbtn">Case <i class="fa fa-caret-down"></i></button>
+      <div id="caseDrop" class="dropdown-content">
+        <input type="text" placeholder="Search.." id="caseInput" onkeyup="filterFunction('caseInput', 'caseDrop')">
+
+      </div>
+    </div>
+
+    <div class="dropdown">
+      <button onclick="myFunction('Power Supply', 'powDrop')" class="dropbtn">Power Supply <i class="fa fa-caret-down"></i></button>
+      <div id="powDrop" class="dropdown-content">
+        <input type="text" placeholder="Search.." id="powInput" onkeyup="filterFunction('powInput', 'powDrop')">
+
+      </div>
+    </div>
+
   </div>
 
-  <div class="dropdown">
-    <button onclick = "myFunction('Motherboard', 'mbDrop')" class="dropbtn">Motherboard <i class="fa fa-caret-down"></i></button>
-    <div id="mbDrop" class="dropdown-content">
-      <input type="text" placeholder="Search.." id="mbInput" onkeyup="filterFunction('mbInput', 'mbDrop')">
 
-    </div>
-  </div>
-
-  <div class="dropdown">
-    <button onclick = "myFunction('Memory', 'mmDrop')" class="dropbtn">Memory <i class="fa fa-caret-down"></i></button>
-    <div id="mmDrop" class="dropdown-content">
-      <input type="text" placeholder="Search.." id="mmInput" onkeyup="filterFunction('mmInput','mmDrop')">
-
-    </div>
-  </div>
-
-  <div class="dropdown">
-    <button onclick = "myFunction('GPU', 'gpuDrop')" class="dropbtn">GPU <i class="fa fa-caret-down"></i></button>
-    <div id="gpuDrop" class="dropdown-content">
-      <input type="text" placeholder="Search.." id="gpuInput" onkeyup="filterFunction('gpuInput','gpuDrop')">
-
-    </div>
-  </div>
-
-  <div class="dropdown">
-    <button onclick = "myFunction('SSD', 'ssdDrop')"class="dropbtn">SSD <i class="fa fa-caret-down"></i></button>
-    <div id="ssdDrop" class="dropdown-content">
-      <input type="text" placeholder="Search.." id="ssdInput" onkeyup="filterFunction('ssdInput', 'ssdDrop')">
-
-    </div>
-  </div>
-
-  <div class="dropdown">
-    <button onclick = "myFunction('HDD', 'hddDrop')"class="dropbtn">HDD <i class="fa fa-caret-down"></i></button>
-    <div id="hddDrop" class="dropdown-content">
-      <input type="text" placeholder="Search.." id="hddInput" onkeyup="filterFunction('hddInput', 'hddDrop')">
-
-    </div>
-  </div>
-
-  <div class="dropdown">
-    <button onclick = "myFunction('Case', 'caseDrop')"class="dropbtn">Case <i class="fa fa-caret-down"></i></button>
-    <div id="caseDrop" class="dropdown-content">
-      <input type="text" placeholder="Search.." id="caseInput" onkeyup="filterFunction('caseInput', 'caseDrop')">
-
-    </div>
-  </div>
-
-  <div class="dropdown">
-    <button onclick = "myFunction('Power Supply', 'powDrop')"class="dropbtn">Power Supply <i class="fa fa-caret-down"></i></button>
-    <div id="powDrop" class="dropdown-content">
-      <input type="text" placeholder="Search.." id="powInput" onkeyup="filterFunction('powInput', 'powDrop')">
-
-    </div>
-  </div>
-
-</div>
-
-
-<table id="t01">
+  <table id="t01">
     <tr>
-        <th>CPU</th>
-        <th>Motherboard</th>
-        <th>Memory</th>
-        <th>GPU</th>
-        <th>SSD</th>
-        <th>HDD</th>
-        <th>Case</th>
-        <th>Power Supply</th>
-        <th style="background:orange">Total Price</th>
+      <th>CPU</th>
+      <th>Motherboard</th>
+      <th>Memory</th>
+      <th>GPU</th>
+      <th>SSD</th>
+      <th>HDD</th>
+      <th>Case</th>
+      <th>Power Supply</th>
+      <th style="background:orange">Total Price</th>
     </tr>
     <tr>
       <td>0.0</td>
@@ -108,7 +108,7 @@
       <td>0.0</td>
       <td>0.0</td>
     </tr>
-</table>
+  </table>
 
 
 
@@ -116,9 +116,9 @@
   <script src="js/jquery-3.3.1.min.js"></script>
 
   <script>
-
-  var header = ["CPU", "Motherboard", "Memory", "GPU", "SSD", "HDD", "Case", "Power Supply", "price"];
-  var price = 0;
+    var header = {"CPU":false, "Motherboard":false, "Memory":false, "GPU":false,
+     "SSD":false, "HDD":false, "Case":false, "Power Supply":false, "price":false};
+    var price = 0;
     //@param cate: the category this button belongs to
     //@param dropId: the Id of the dropdown-connent
     // TODO:
@@ -126,48 +126,57 @@
     function myFunction(cate, dropId) {
       // Insert the SKU of this categroy from the database into the dropdown
       // pass the category to the database
+      if(!header[cate]) {
       $.ajax({
-          url: "basicFuncCon/skuSearch",
-          data: {cate:cate},
-          datatype:'json',
-          success: function(data) {
-            // return all the skus of this category and parse them into $parsedData
-            var parsedData = JSON.parse(data);
-            if(parsedData.length > 0) {
-              console.log("SKU initialization successful");
-              // Insert each sku into the dropdown content.
-              parsedData.forEach((item, i) => {
-                  div = document.getElementById(dropId);
-                  // for each sku we get from db we create a new empty link
-                  t = document.createElement("a");
-                  // the text the link shows is the sku
-                  t.innerHTML = item.sku;
-                  // Once click on the link
-                  t.onclick = function() {
-                    // update the corresonding header with sku in yellow
-                    thead = document.getElementsByTagName('th');
-                    cell = thead[header.indexOf(cate)]
-                    cell.innerHTML = cate + " : ";
-                    var skuSpan = document.createElement("span");
-                    skuSpan.textContent = item.sku;
-                    skuSpan.style = "color:yellow";
-                    cell.appendChild(skuSpan);
+        url: "basicFuncCon/skuSearch",
+        data: {
+          cate: cate
+        },
+        datatype: 'json',
+        success: function(data) {
+          // return all the skus of this category and parse them into $parsedData
+          var parsedData = JSON.parse(data);
+          if (parsedData.length > 0) {
+            console.log("SKU initialization successful");
+            // Insert each sku into the dropdown content.
+            parsedData.forEach((item, i) => {
+              div = document.getElementById(dropId);
+              // for each sku we get from db we create a new empty link
+              t = document.createElement("a");
+              // the text the link shows is the sku
+              t.innerHTML = item.sku;
+              // Once click on the link
+              t.onclick = function() {
+                // update the corresonding header with sku in yellow
+                thead = document.getElementsByTagName('th');
+                cell = thead[Object.keys(header).indexOf(cate)];
+                cell.innerHTML = cate + " : ";
+                var skuSpan = document.createElement("span");
+                skuSpan.textContent = item.sku;
+                skuSpan.style = "color:yellow";
+                cell.appendChild(skuSpan);
 
 
-                    // search for the price of corresonding sku from db and update the price cell
-                    priceSearch(cate, item.sku);
+                // search for the price of corresonding sku from db and update the price cell
+                priceSearch(cate, item.sku);
 
 
-                  };
+              };
 
-                  // we append the link into the dropdown-content
-                  div.appendChild(t);
-              });
-              // alert if the request of skus failed.
-            } else {
-              alert("SKU initialization Failed")
-            }
-          }});
+              // we append the link into the dropdown-content
+
+              div.appendChild(t);
+
+
+            });
+            // alert if the request of skus failed.
+          } else {
+            alert("SKU initialization Failed")
+          }
+        }
+      });
+        header[cate] = true;
+    }
 
       // show the dropdown content
       document.getElementById(dropId).classList.toggle("show");
@@ -177,22 +186,25 @@
       input = document.getElementById(inputId);
       console.log(inputId);
     }
-     //@param cate: the category the sku belongs to
-     //@param sku: the sku we want to search for price
+    //@param cate: the category the sku belongs to
+    //@param sku: the sku we want to search for price
     function priceSearch(cate, sku) {
       $.ajax({
-          url: "basicFuncCon/priceSearch",
-          data: {sku:sku},
-          success: function(data) {
+        url: "basicFuncCon/priceSearch",
+        data: {
+          sku: sku
+        },
+        success: function(data) {
           // returns the price of the product with the sku privided.
           tData = document.getElementsByTagName('td');
-          tData[header.indexOf(cate)].innerHTML = data;
+          tData[Object.keys(header).indexOf(cate)].innerHTML = data;
           price = 0;
-          for(var i = 0; i < header.length -1; i++) {
+          for (var i = 0; i < header.length - 1; i++) {
             price += parseFloat(tData[i].innerHTML);
           }
-          tData[header.indexOf("price")].innerHTML = price;
-          }});
+          tData[Object.keys(header).indexOf("price")].innerHTML = price;
+        }
+      });
     }
 
 
